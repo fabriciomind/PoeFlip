1e6f48bec3e76b0c1350fab7fed3e917702f7309
Fabrício <fabriciomind@gmail.com>
2017-01-27 00:43:54 -0200

    Initial commit

---
+++ Flipper/src/com/asgornoth/poeflip/app/App.java
@@ -0,0 +1,12 @@
+package com.asgornoth.poeflip.app;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
